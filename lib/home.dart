a5d9250e62c3beda2e49de613c2ccddae691d91e
sh183694@gmail.com <sh183694@gamil.com>
2019-08-27 16:44:11 +0900

    changeUI

--- lib/home.dart
+++ lib/home.dart
@@ -55,29 +55,66 @@ class _HomePageState extends State<HomePage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: <Widget>[
                       FutureBuilder<Post>(
                         future: refresh,
                         builder: (context, snapshot) {
                           if (snapshot.hasData) {
-                            if (snapshot.data.filter <= 2000) {
-                              return SizedBox(
-                                width: 100,
-                                height: 160,
-                                child: Card(color: Colors.red),
+                            int water = snapshot.data.water;
+
+                            return Text(
+                              '오늘의 정수량 : \n' + water.toString(),
+                              textScaleFactor: 1.0,
+                              textAlign: TextAlign.center,
+                            );
+                          } else if (snapshot.hasError) {
+                            return Text("${snapshot.error}");
+                          }
+                          // By default, show a loading spinner.
+                          return CircularProgressIndicator();
+                        },
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                      ),
+                      FutureBuilder<Post>(
+                        future: refresh,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            if (snapshot.data.water <= 1000) {
+                              return Container(
+                                width: 80,
+                                height: 140,
+                                alignment: Alignment.bottomCenter,
+                                child: SizedBox(
+                                  width: 80,
+                                  height: 60,
+                                  child: Card(color: Colors.lightBlue[200]),
+                                ),
                               );
-                            } else if (snapshot.data.filter > 2000 &&
-                                snapshot.data.filter <= 4000) {
-                              return SizedBox(
-                                width: 100,
-                                height: 160,
-                                child: Card(color: Colors.yellow),
+                            } else if (snapshot.data.water > 1000 &&
+                                snapshot.data.water <= 2000) {
+                              return Container(
+                                width: 80,
+                                height: 140,
+                                alignment: Alignment.bottomCenter,
+                                child: SizedBox(
+                                  width: 80,
+                                  height: 100,
+                                  child: Card(color: Colors.lightBlue[200]),
+                                ),
                               );
                             } else {
-                              return SizedBox(
-                                width: 100,
-                                height: 160,
-                                child: Card(color: Colors.green),
+                              return Container(
+                                width: 80,
+                                height: 140,
+                                alignment: Alignment.bottomCenter,
+                                child: SizedBox(
+                                  width: 80,
+                                  height: 140,
+                                  child: Card(color: Colors.lightBlue[200]),
+                                ),
                               );
                             }
                           } else if (snapshot.hasError) {
@@ -106,11 +143,12 @@ class _HomePageState extends State<HomePage> {
                         future: refresh,
                         builder: (context, snapshot) {
                           if (snapshot.hasData) {
-                            int water = snapshot.data.water;
+                            int filter = snapshot.data.filter;
 
                             return Text(
-                              'Water Size : ' + water.toString(),
-                              textScaleFactor: 2.0,
+                              'Filter : \n' + filter.toString(),
+                              textScaleFactor: 1.0,
+                              textAlign: TextAlign.center,
                             );
                           } else if (snapshot.hasError) {
                             return Text("${snapshot.error}");
@@ -126,16 +164,37 @@ class _HomePageState extends State<HomePage> {
                         future: refresh,
                         builder: (context, snapshot) {
                           if (snapshot.hasData) {
-                            bool notify = snapshot.data.notify;
-                            return Text(
-                              'Status : ' + notify.toString(),
-                              textScaleFactor: 2.0,
-                            );
+                            if (snapshot.data.filter <= 2000) {
+                              return SizedBox(
+                                width: 80,
+                                height: 140,
+                                child: Card(color: Colors.red),
+                              );
+                            } else if (snapshot.data.filter > 2000 &&
+                                snapshot.data.filter <= 4000) {
+                              return SizedBox(
+                                width: 80,
+                                height: 140,
+                                child: Card(color: Colors.yellow),
+                              );
+                            } else {
+                              return SizedBox(
+                                width: 80,
+                                height: 140,
+                                child: Card(color: Colors.green),
+                              );
+                            }
                           } else if (snapshot.hasError) {
                             return Text("${snapshot.error}");
                           }
                           // By default, show a loading spinner.
                           return CircularProgressIndicator();
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text('filter'),
+                        onPressed: () {
+                          emailSend();
                         },
                       ),
                     ],
@@ -145,10 +204,20 @@ class _HomePageState extends State<HomePage> {
               Padding(
                 padding: EdgeInsets.all(8.0),
               ),
-              RaisedButton(
-                child: Text('filter'),
-                onPressed: () {
-                  emailSend();
+              FutureBuilder<Post>(
+                future: refresh,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    bool notify = snapshot.data.notify;
+                    return Text(
+                      'SilverCare : ' + notify.toString(),
+                      textScaleFactor: 1.0,
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+                  // By default, show a loading spinner.
+                  return CircularProgressIndicator();
                 },
               ),
             ],
